9b637d82ee0918a02bfb30de7567f1954f29b422
PatrickFreyy <patrick.langenalb@gmail.com>
2022-04-28 21:12:12 +0200

    JunitTest

--- src/main/java/test/TestFilePersistence.java
+++ src/main/java/test/TestFilePersistence.java
@@ -29,8 +29,7 @@ public class TestFilePersistence {
 	public void createTestFile() throws IOException {
 		fp = new PersistenceAppointmentModel(System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Appointments by PlanIt",
 				 File.separator + "appointmentModel_Test.json");
-		
-		List<AppointmentModel> appointmentList = new ArrayList<>();
+//		generiert Test-Fälle
 		fp.add(new AppointmentModel("12", "Friseur", "2022-03-14T14-00-00", "2022-03-14T15-00-00", ""));
 		fp.add(new AppointmentModel("85", "Geburtstag", "2022-03-31T06-00-00", "2022-03-31T14-00-00", ""));
 		fp.add(new AppointmentModel("123", "Zahnarzt", "2022-03-01T12-00-00","2022-03-31T14-00-00", ""));
@@ -92,7 +91,15 @@ public class TestFilePersistence {
 
 	@Ignore("Not implemented yet")
 	@Test
-	public void testLoadAppointments() {
+	public void testLoadAppointments() throws IOException {
+		List<AppointmentModel> list = new ArrayList<>();
+		list.add(new AppointmentModel("12", "Friseur", "2022-03-14T14-00-00", "2022-03-14T15-00-00", ""));
+		list.add(new AppointmentModel("85", "Geburtstag", "2022-03-31T06-00-00", "2022-03-31T14-00-00", ""));
+		list.add(new AppointmentModel("123", "Zahnarzt", "2022-03-01T12-00-00","2022-03-31T14-00-00", ""));
+		list.add(new AppointmentModel("1020", "Abendessen", "2022-03-25T21-00-00", "2022-03-26T01-00-00", ""));
+		list.add(new AppointmentModel("45605", "Silvesterfeier", "2022-12-31T23-00-00", "2023-01-01T01-00-00", ""));
+		list.add(new AppointmentModel("9874", "Urlaub", "2022-03-28T06-00-00", "2022-04-15T23-59-59", ""));
+		assertEquals(fp.load(), list);
 	}
 
 	@Ignore("Not implemented yet")
