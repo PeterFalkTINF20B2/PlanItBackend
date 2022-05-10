a6caa2aa0e3b3734b57ae9ec436e7539aab7a548
PatrickFreyy <patrick.langenalb@gmail.com>
2022-05-10 11:48:46 +0200

    loadInTimeSpan Fehler behoben

--- src/main/java/main/AppointmentModel.java
+++ src/main/java/main/AppointmentModel.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class AppointmentModel {
 	private String id; // generated
