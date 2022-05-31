b1b020ef0a35e1c8b2fedfbb9732fb31385f32ca
Jennifer <jennifer.zaminer@gmail.com>
2022-05-31 21:55:07 +0200

    restructure Calendar

--- src/main/java/main/Calendar.java
+++ src/main/java/main/Calendar.java
@@ -3,24 +3,17 @@ package main;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.Date;
 
 public class Calendar {
     public Calendar(){
     }
 
     public static String getDayOfWeekBefore(String dateString){
-//        @SuppressWarnings("deprecation")
-//		LocalDate oldDate = LocalDate.parse(dateString);
-//        LocalDate newDate = oldDate.minusDays(7);
-//        return newDate.toString();
     	return LocalDate.parse(dateString).minusDays(7).toString();
     }
     
     public static String getDayofWeekAfter(String dateString) {
-    	LocalDate oldDate = LocalDate.parse(dateString);
-    	LocalDate newDate = oldDate.plusDays(7);
-    	return newDate.toString();
+    	return LocalDate.parse(dateString).plusDays(7).toString();
     }
     
     public static List<String> getAktualWeek(String date){
@@ -28,8 +21,7 @@ public class Calendar {
     	FilePersistenceModel fpm = new FilePersistenceModel("", "");
     	LocalDate startDate = fpm.getMonday(LocalDate.parse(date));
     	week.add(startDate.toString());
-		LocalDate endDate = startDate.plusDays(6);
-		week.add(endDate.toString());
+		week.add(startDate.plusDays(6).toString());
     	return week;
     }
 }
