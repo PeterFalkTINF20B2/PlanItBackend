4881c1381b1200ce52c60ae63662d2f442f2dcde
PeterFalkTINF20B2 <85633848+PeterFalkTINF20B2@users.noreply.github.com>
2022-06-12 20:38:41 +0200

    Refactor + unused classes deleted

--- src/main/java/main/Calendar.java
+++ src/main/java/main/Calendar.java
@@ -5,23 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calendar {
-    public Calendar(){
-    }
 
-    public static String getDayOfWeekBefore(String dateString){
-    	return LocalDate.parse(dateString).minusDays(7).toString();
-    }
-    
-    public static String getDayofWeekAfter(String dateString) {
-    	return LocalDate.parse(dateString).plusDays(7).toString();
-    }
-    
-    public static List<String> getAktualWeek(String date){
-    	List<String> week = new ArrayList<String>();
-    	FilePersistenceModel fpm = new FilePersistenceModel("", "");
-    	LocalDate startDate = fpm.getMonday(LocalDate.parse(date));
-    	week.add(startDate.toString());
+	public static String getDayOfWeekBefore(String dateString) {
+		return LocalDate.parse(dateString).minusDays(7).toString();
+	}
+
+	public static String getDayofWeekAfter(String dateString) {
+		return LocalDate.parse(dateString).plusDays(7).toString();
+	}
+
+	public static List<String> getAktualWeek(String date) {
+		List<String> week = new ArrayList<String>();
+		FilePersistenceModel fpm = new FilePersistenceModel("", "");
+		LocalDate startDate = fpm.getMonday(LocalDate.parse(date));
+		week.add(startDate.toString());
 		week.add(startDate.plusDays(6).toString());
-    	return week;
-    }
+		return week;
+	}
 }
--- src/main/java/main/Color.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public enum Color {
-      RED, GREEN, BLUE, YELLOW;
-}
